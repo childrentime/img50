31bbbeb3ebe2d37afd4ce4f7464849faf08316c7
儿时 <58261676+childrentime@users.noreply.github.com>
2022-03-30 14:07:32 +0800

    add ovverriew

---
+++ src/pages/img50/index.tsx
@@ -0,0 +1,12 @@
+import Header from "../../components/Header";
+import Style from "./style.module.css";
+
+const Img50 = () => {
+  return (
+    <div className={Style.page}>
+      <Header />
+    </div>
+  );
+};
+
+export default Img50;
