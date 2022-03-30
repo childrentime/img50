4f749258c8306082776d95e9604b16ae479f6c60
儿时 <58261676+childrentime@users.noreply.github.com>
2022-03-30 16:06:34 +0800

    add container text

---
+++ src/pages/img50/containerText/index.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useRef } from "react";
+import { Button } from "antd";
+import Style from "./style.module.css";
+const ContainerText = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    setTimeout(() => {
+      containerRef.current!.classList.add(`${Style.ready}`);
+    }, 500);
+  }, []);
+  return (
+    <div className={`${Style.containerText}`} ref={containerRef}>
+      <h1 className={Style.h1}>Super Photo Brows</h1>
+      <p className={Style.text}>
+        Img50% is a network platform that provides precise algorithm functions
+        for image processing enthusiasts. Thrive digitally as we improve your
+        pictures with super image manipulation tools.
+      </p>
+      <div className={Style.containerButton}>
+        <Button
+          shape="round"
+          style={{ color: "white", backgroundColor: "#224279" }}
+        >
+          Try now for free
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default ContainerText;
