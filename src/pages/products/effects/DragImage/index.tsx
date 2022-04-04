41b28df28b2db943f8a96a164ddc89fd0f005e35
儿时 <58261676+childrentime@users.noreply.github.com>
2022-04-05 00:00:03 +0800

    fix drag  bug

--- src/pages/products/effects/DragImage/index.tsx
+++ src/pages/products/effects/DragImage/index.tsx
@@ -3,22 +3,24 @@ import Style from "./style.module.css";
 const DragImage = (props: { url: string; urlC: string }) => {
   const { url, urlC } = props;
   const [width, setWidth] = useState(0);
+  const [max, setMax] = useState(0);
   const compareRef = useRef<HTMLDivElement>(null);
   const divideRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
     const init = Math.round(compareRef.current!.getBoundingClientRect().left);
+    const width = compareRef.current!.getBoundingClientRect().width;
     compareRef.current!.onmousemove = (event: MouseEvent) => {
       setWidth(event.pageX - init);
     };
     compareRef.current!.ontouchmove = (event: TouchEvent) => {
-      const max = 334;
       const v = event.touches[0].pageX - init;
-      if (v > max) {
-        setWidth(max);
+      if (v > width) {
+        setWidth(width);
       } else {
         setWidth(v);
       }
     };
+    setMax(width);
   }, []);
   return (
     <div className={Style.comparator} ref={compareRef}>
@@ -31,11 +33,11 @@ const DragImage = (props: { url: string; urlC: string }) => {
         className={`${Style.before} ${Style.content}`}
         style={{ width: `${width}px` }}
       >
-        <img src={urlC} alt="" className={Style.img} />
+        <img src={urlC} alt="" style={{ width: `${max}px` }} />
       </div>
 
       <div className={`${Style.after} ${Style.content}`}>
-        <img src={url} alt="" className={Style.img} />
+        <img src={url} alt="" style={{ width: `${max}px` }} />
       </div>
     </div>
   );
