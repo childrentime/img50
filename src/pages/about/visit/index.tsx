dbd0aa9350c50d884d4484d6615237932b859515
儿时 <58261676+childrentime@users.noreply.github.com>
2022-04-08 14:33:48 +0800

    add about

--- src/pages/about/visit/index.tsx
+++ src/pages/about/visit/index.tsx
@@ -7,6 +7,28 @@ const Visit = () => {
         No.1 Textile Road, Minzu Avenue, Hongshan District, Wuhan City, Hubei
         Province
       </div>
+      <div className={Style.mapContainer}>
+        <div className={Style.map}>
+          <div className={Style.point}>
+            <div title="Australia" className={Style.tip}></div>
+          </div>
+          <div className={Style.point}>
+            <div title="Belarus" className={Style.tip}></div>
+          </div>
+          <div className={`${Style.point} ${Style.pointM}`}>
+            <div title="Bosnia" className={Style.tip}></div>
+          </div>
+          <div className={`${Style.point} ${Style.pointXL}`}>
+            <div title="United States" className={Style.tip}></div>
+          </div>
+          <div className={`${Style.point} ${Style.pointL}`}>
+            <div title="Poland" className={Style.tip}></div>
+          </div>
+          <div className={`${Style.point} ${Style.pointM}`}>
+            <div title="Brazil" className={Style.tip}></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
